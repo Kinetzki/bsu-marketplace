e6713a0f9e34035fc1576255e916b9af68d2b833
Kenneth <kinetzki2k20@gmail.com>
2024-03-22 12:28:52 +0800

    fix: dashcard leaning space

--- src/components/DashCard.jsx
+++ src/components/DashCard.jsx
@@ -7,8 +7,8 @@ function DashCard({ title, content, description, icon }) {
       otherStyle={"flex justify-between px-8 py-1 items-center min-w-[330px]"}
     >
       <div className="flex flex-col font-Archivo">
-        <h1 className="text-[var(--accent-color)] text-[13px]">{title}</h1>
-        <h1 className="text-[16px]">{content}</h1>
+        <h1 className="text-[var(--accent-color)] text-[13px] ">{title}</h1>
+        <h1 className="text-[16px] leading-none">{content}</h1>
         <h1 className="text-[var(--accent-color)]">{description}</h1>
       </div>
       <img src={icon} alt="" className="w-[50px] h-[50px]" />
