dc45e55d44b0d38f74ca3efd6a706813761fb267
Kenneth <kinetzki2k20@gmail.com>
2024-03-24 16:52:49 +0800

    feat: loader on all panels

--- src/panels/History.jsx
+++ src/panels/History.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Banner from '../components/Banner'
 import Label1 from '../components/Label1'
+import ScreenLoader from '../components/ScreenLoader';
 
 function History() {
+  
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
   return (
-    <div className="w-full min-h-screen flex flex-col">
+    <div className="w-full min-h-screen flex flex-col relative">
+      {isLoading && <ScreenLoader otherstyle={"z-[2]"} />}
       <Banner title="History"/>
       <Label1 text={"No History"}/>
     </div>
--- src/panels/Items.jsx
+++ src/panels/Items.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import Banner from '../components/Banner'
-import Label1 from '../components/Label1'
+import React, { useEffect, useState } from "react";
+import Banner from "../components/Banner";
+import Label1 from "../components/Label1";
+import ScreenLoader from "../components/ScreenLoader";
 
 function Items() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
   return (
-    <div className="w-full min-h-screen flex flex-col">
-      <Banner title="Items"/>
-      <Label1 text="No items "/>
+    <div className="w-full min-h-screen flex flex-col relative">
+      {isLoading && <ScreenLoader otherstyle={"z-[2]"} />}
+      <Banner title="Items" />
+      <Label1 text="No items " />
     </div>
-  )
+  );
 }
 
-export default Items
+export default Items;
